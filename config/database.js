430f49001ec6aabf65fa3ee5cbfd8ead7f6b9f6d
Yudi Krisnandi <yudikrisnandi22@gmail.com>
2022-09-26 08:30:55 +0700

    connect to railway db

--- config/database.js
+++ config/database.js
@@ -1,16 +1,8 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv').config();
 
-const { 
-  DB_USER_NAME,
-  DB_HOST,
-  DB_PASSWORD,
-  DB_TABLE
-} = process.env
+const { DB_URL } = process.env
 
-const db = new Sequelize(DB_TABLE, DB_USER_NAME, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres'
-});
+const db = new Sequelize(DB_URL)
 
 module.exports = db;
